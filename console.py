3a79b3efd8dc9e99101db85a1da1722de6877dcd
robberbadeia <robber.badeia@gmail.com>
2023-12-05 22:19:11 +0200

    T07 Create Method

--- console.py
+++ console.py
@@ -4,10 +4,12 @@ contains the entry point of the command interpreter
 """
 
 import cmd
-
+from models.base_model import BaseModel
 
 class HBNBCommand(cmd.Cmd):
     """Class Implementation"""
+
+    class_dic = {"BaseModel" : BaseModel}
 
     def __init__(self):
         """Initiation Function"""
@@ -28,6 +30,21 @@ class HBNBCommand(cmd.Cmd):
     def emptyline(self):
         pass
 
+
+    def do_create(self, line):
+        """Creates a new instance of BaseModel"""
+
+        args_lst = line.split()
+        if len(args_lst) == 0:
+            print ("** class name missing **")
+            return False
+        elif args_lst[0] not in self.class_dic:
+            print ("** class doesn't exist **")
+            return False
+        else:
+            instance = eval(line)()
+            instance.save()
+            print(instance.id)
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
