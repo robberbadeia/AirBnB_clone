afb9fd7345f265ef5a4c504a2b56e8b4849989cc
Abdulrahman Alkhateeb <abdoh.alkhateeb@gmail.com>
2023-12-06 18:11:23 +0200

    Update models/base_model.py to refactor import statement

--- models/base_model.py
+++ models/base_model.py
@@ -5,7 +5,7 @@ Module that defines BaseModel class.
 
 import uuid
 from datetime import datetime
-import models
+from models import storage
 
 
 class BaseModel:
@@ -31,7 +31,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            models.storage.new(self)
+            storage.new(self)
 
     def __str__(self):
         """
@@ -46,7 +46,7 @@ class BaseModel:
         """
 
         self.updated_at = datetime.now()
-        models.storage.save()
+        storage.save()
 
     def to_dict(self):
         """
