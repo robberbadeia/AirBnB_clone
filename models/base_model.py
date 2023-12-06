63b76bfcdb6bbe37589cb3335ddf23926266992d
Abdulrahman Alkhateeb <abdoh.alkhateeb@gmail.com>
2023-12-06 23:08:24 +0200

    Update models/base_model.py to fix import issue

--- models/base_model.py
+++ models/base_model.py
@@ -5,7 +5,7 @@ Module that defines BaseModel class.
 
 import uuid
 from datetime import datetime
-# from models import storage
+import models
 
 
 class BaseModel:
@@ -31,7 +31,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            storage.new(self)
+            models.storage.new(self)
 
     def __str__(self):
         """
@@ -46,7 +46,7 @@ class BaseModel:
         """
 
         self.updated_at = datetime.now()
-        storage.save()
+        models.storage.save()
 
     def to_dict(self):
         """
