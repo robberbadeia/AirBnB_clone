ec3a08b9a5c303d3c1e005202825eb368c360040
Abdulrahman Alkhateeb <abdoh.alkhateeb@gmail.com>
2023-12-11 00:37:30 +0200

    Update tests/test_models/test_engine/test_file_storage.py in an attempt to make tests more rigorous

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -8,6 +8,12 @@ import json
 import unittest
 from models.engine.file_storage import FileStorage
 from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
 
 
 class TestFileStorage(unittest.TestCase):
@@ -28,6 +34,8 @@ class TestFileStorage(unittest.TestCase):
         Method that cleans up environment after each test.
         """
 
+        FileStorage._FileStorage__objects = {}
+
         try:
             os.remove("file.json")
         except FileNotFoundError:
@@ -46,11 +54,37 @@ class TestFileStorage(unittest.TestCase):
         """
 
         storage = self.__class()
-        base_model = BaseModel()
-        base_model.id = 1488
-        storage.new(base_model)
-        key = base_model.__class__.__name__ + "." + str(base_model.id)
-        self.assertIn(key, storage.all())
+
+        b = BaseModel()
+        u = User()
+        s = State()
+        c = City()
+        a = Amenity()
+        p = Place()
+        r = Review()
+
+        storage.new(b)
+        storage.new(u)
+        storage.new(s)
+        storage.new(c)
+        storage.new(a)
+        storage.new(p)
+        storage.new(r)
+
+        self.assertIn("BaseModel." + b.id, storage.all())
+        self.assertIn(b, storage.all().values())
+        self.assertIn("User." + u.id, storage.all())
+        self.assertIn(u, storage.all().values())
+        self.assertIn("State." + s.id, storage.all())
+        self.assertIn(s, storage.all().values())
+        self.assertIn("City." + c.id, storage.all())
+        self.assertIn(c, storage.all().values())
+        self.assertIn("Amenity." + a.id, storage.all())
+        self.assertIn(a, storage.all().values())
+        self.assertIn("Place." + p.id, storage.all())
+        self.assertIn(p, storage.all().values())
+        self.assertIn("Review." + r.id, storage.all())
+        self.assertIn(r, storage.all().values())
 
     def test_save(self):
         """
@@ -58,29 +92,69 @@ class TestFileStorage(unittest.TestCase):
         """
 
         storage = self.__class()
-        obj = BaseModel()
-        storage.new(obj)
+
+        b = BaseModel()
+        u = User()
+        s = State()
+        c = City()
+        a = Amenity()
+        p = Place()
+        r = Review()
+
+        storage.new(b)
+        storage.new(u)
+        storage.new(s)
+        storage.new(c)
+        storage.new(a)
+        storage.new(p)
+        storage.new(r)
+
         storage.save()
-        self.assertTrue(os.path.exists("file.json"))
+
         with open("file.json", "r") as f:
-            self.assertEqual(json.load(f),
-                             {k: v.to_dict() for k, v in storage.all().items()}
-                             )
+            save = f.read()
+            self.assertIn("BaseModel." + b.id, save)
+            self.assertIn("User." + u.id, save)
+            self.assertIn("State." + s.id, save)
+            self.assertIn("City." + c.id, save)
+            self.assertIn("Amenity." + a.id, save)
+            self.assertIn("Place." + p.id, save)
+            self.assertIn("Review." + r.id, save)
 
     def test_reload(self):
         """
         Method that tests reload method.
         """
 
-        obj = BaseModel()
-        with open("file.json", "w") as f:
-            f.write("{ " + f'"BaseModel.{obj.id}": ' +
-                    json.dumps(obj.to_dict()) + " }")
-
         storage = self.__class()
+
+        b = BaseModel()
+        u = User()
+        s = State()
+        c = City()
+        a = Amenity()
+        p = Place()
+        r = Review()
+
+        storage.new(b)
+        storage.new(u)
+        storage.new(s)
+        storage.new(c)
+        storage.new(a)
+        storage.new(p)
+        storage.new(r)
+
+        storage.save()
         storage.reload()
 
-        self.assertIn(f"BaseModel.{obj.id}", storage.all())
+        objects = FileStorage._FileStorage__objects
+        self.assertIn("BaseModel." + b.id, objects)
+        self.assertIn("User." + u.id, objects)
+        self.assertIn("State." + s.id, objects)
+        self.assertIn("Place." + p.id, objects)
+        self.assertIn("City." + c.id, objects)
+        self.assertIn("Amenity." + a.id, objects)
+        self.assertIn("Review." + r.id, objects)
 
 
 if __name__ == "__main__":
